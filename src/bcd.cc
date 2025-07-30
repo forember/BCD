877dbdebccd22bd46699674651dd461d649f30f7
Emberlynn McKinney <em@embermckinney.com>
2025-07-30 02:36:40 -0400

    cull overhangs

--- src/bcd.cc
+++ src/bcd.cc
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <argp.h>
 #include <iostream>
 #include <png.h>
@@ -64,6 +65,105 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
 
 static struct argp argp = { options, parse_opt, args_doc, doc };
 
+void invalidate_start_merge(BCDEvent &a, size_t x,
+    std::vector<std::vector<BCDEvent>> &events, int leeway)
+{
+    slice &a_slice = a.right.at(0);
+    for (size_t i = 1; i <= leeway && x + i < events.size(); ++i)
+    {
+        for (BCDEvent &b : events.at(x + i))
+        {
+            for (size_t j = 0; j < b.left.size(); ++j)
+            {
+                slice &b_slice = b.left.at(j);
+                if (a_slice.first < b_slice.second && b_slice.first < a_slice.second)
+                {
+                    if (b.event_type == BCD_EVENT_MERGE)
+                    {
+                        a.left.clear();
+                        a.right.clear();
+                        if (b.left.size() <= 2)
+                        {
+                            b.left.clear();
+                            b.right.clear();
+                        }
+                        else
+                        {
+                            b.left.erase(b.left.begin() + j);
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
+
+void invalidate_split_end(BCDEvent &a, size_t x,
+    std::vector<std::vector<BCDEvent>> &events, int leeway)
+{
+    slice &a_slice = a.left.at(0);
+    for (size_t i = 1; i <= leeway && x - i >= 0; ++i)
+    {
+        for (BCDEvent &b : events.at(x - i))
+        {
+            for (size_t j = 0; j < b.right.size(); ++j)
+            {
+                slice &b_slice = b.right.at(j);
+                if (a_slice.first < b_slice.second && b_slice.first < a_slice.second)
+                {
+                    if (b.event_type == BCD_EVENT_SPLIT)
+                    {
+                        a.left.clear();
+                        a.right.clear();
+                        if (b.right.size() <= 2)
+                        {
+                            b.left.clear();
+                            b.right.clear();
+                        }
+                        else
+                        {
+                            b.right.erase(b.right.begin() + j);
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
+
+void invalidate_overhangs(std::vector<std::vector<BCDEvent>> &events,
+    int leeway)
+{
+    for (size_t x = 0; x < events.size(); ++x)
+    {
+        for (BCDEvent &a : events.at(x))
+        {
+            if (a.event_type == BCD_EVENT_START)
+            {
+                invalidate_start_merge(a, x, events, leeway);
+            }
+            else if (a.event_type == BCD_EVENT_END)
+            {
+                invalidate_split_end(a, x, events, leeway);
+            }
+            
+        }
+    }
+}
+
+void erase_invalid(std::vector<std::vector<BCDEvent>> &events)
+{
+    for (auto &column : events)
+    {
+        column.erase(std::remove_if(column.begin(), column.end(),
+            [](BCDEvent &event) {
+                return event.left.empty() && event.right.empty();
+            }), column.end());
+    }
+}
+
 void color_event(const BCDEvent &event, size_t x,
     std::vector<std::vector<struct color>> &color_columns)
 {
@@ -71,16 +171,20 @@ void color_event(const BCDEvent &event, size_t x,
     switch (event.event_type)
     {
         case BCD_EVENT_START:
-            color.g = 0xff;
+            color.g = 0x99;
+            color.b = 0xff;
             break;
         case BCD_EVENT_END:
-            color.r = 0xff;
+            color.r = 0x99;
+            color.b = 0xff;
             break;
         case BCD_EVENT_MERGE:
-            color.g = color.b = 0xff;
+            color.g = 0xff;
+            color.b = 0x99;
             break;
         case BCD_EVENT_SPLIT:
-            color.r = color.b = 0xff;
+            color.r = 0xff;
+            color.b = 0x99;
             break;
     }
     for (const slice &a : event.left)
@@ -120,7 +224,9 @@ int main(int argc, char **argv)
     size_t height = binary_columns.at(0).size();
     binary_columns.emplace(binary_columns.begin(), height, false);
     binary_columns.emplace_back(height, false);
-    const auto events = find_all_events(binary_columns, true);
+    auto events = find_all_events(binary_columns, true);
+    invalidate_overhangs(events, arguments.leeway);
+    erase_invalid(events);
     size_t width = binary_columns.size();
     std::vector<std::vector<struct color>> color_columns(width,
         std::vector<struct color>(height, {0x00, 0x00, 0x00}));
